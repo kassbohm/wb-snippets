84e698a79a9e9e982ccfe19a7a815f0b78f9da67
kassbohm <kassbohm@gmail.com>
2020-03-07 07:13:57 +0100

    "tag"

---
+++ py/TEM_10/ESA/a1.py
@@ -0,0 +1,30 @@
+from sympy import *
+
+F,l = var("F,l")
+
+Ah,Av,Bh,Bv,Ch,Cv = var("Ah,Av,Bh,Bv,Ch,Cv")
+
+# Correct:
+R = 3*F/2
+
+lu = l/sqrt(3)
+
+# left:
+e1 = Eq(Ah + Bh + F)
+e2 = Eq(Av + Bv - R)
+e3 = Eq(Bv*l - Bh*l - F*l/2 - R*7/18*l)
+
+e4 = Eq(Ch - Bh)
+e5 = Eq(Cv - F - Bv)
+e6 = Eq(F*lu/2 + Bv*lu + Bh*l)
+
+eqs = [e1,e2,e3,e4,e5,e6]
+unknowns = [Ah,Av,Bh,Bv,Ch,Cv]
+sol = solve(eqs,unknowns)
+
+pprint("Reactions / F:")
+for v in  sorted(sol,key=default_sort_key):
+    pprint("\n\n")
+    s = sol[v]
+    sF = (s/F).simplify()
+    pprint([v, sF, N(sF,2)])
