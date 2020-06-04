063d60e5c98de4dfb7d30bae8c9b99b099549416
kassbohm <kassbohm@gmail.com>
2020-06-04 11:59:46 +0200

    "tag"

--- py/WB-Klein/5/5.9.py
+++ py/WB-Klein/5/5.9.py
@@ -1,44 +1,6 @@
-EI = 1 *Newton*m**2
-l = 1 *m
-l2 = l*l
-l3 = l*l*l
-
-K = EI/l**3
-K *= Matrix([
-[  18 ,  6*l, -3*l  ],
-[  6*l, 4*l2,  0    ],
-[ -3*l,   0,  2*l2  ],
-])
-
-pprint("\nStiffness matrix / N:")
-pprint(K/Newton)
-
-w2, p2m, p2p = var("w2, p2m, p2p")
-u = Matrix([w2, p2m, p2p])
-f = Matrix([1*Newton,0,0])
-
-eq = Eq(K*u , f)
-sol = solve(eq, [w2, p2m, p2p])
+# https://colab.research.google.com/github/kassbohm/wb-snippets/blob/master/ipynb/WB-Klein/5/5.9_cc.ipynb
 
 pprint("\nSolution 1:")
-pprint(sol)
-
-# Stiffness matrix / N:
-# ⎡18          ⎤
-# ⎢──   6   -3 ⎥
-# ⎢m           ⎥
-# ⎢            ⎥
-# ⎢6   4⋅m   0 ⎥
-# ⎢            ⎥
-# ⎣-3   0   2⋅m⎦
-#
-# Solution:
-# ⎧                         2⋅m⎫
-# ⎨p2m: -1/3, p2p: 1/3, w₂: ───⎬
-# ⎩                          9 ⎭
-
-
-pprint("\nSolution 2:")
 A, F = var("A, F")
 EI, l = var("EI, l")
 sub_list = [
@@ -74,3 +36,57 @@ pprint("\nw / m:")
 w = w.subs(sub_list)
 w /= m
 pprint(w)
+
+pprint("\nSolution 2:")
+
+EI = 1 *Newton*m**2
+l = 1 *m
+l2 = l*l
+l3 = l*l*l
+
+K = EI/l**3
+K *= Matrix([
+[  18 ,  6*l, -3*l  ],
+[  6*l, 4*l2,  0    ],
+[ -3*l,   0,  2*l2  ],
+])
+
+pprint("\nStiffness matrix / N:")
+pprint(K/Newton)
+
+w2, p2m, p2p = var("w2, p2m, p2p")
+u = Matrix([w2, p2m, p2p])
+f = Matrix([1*Newton,0,0])
+
+eq = Eq(K*u , f)
+sol = solve(eq, [w2, p2m, p2p])
+
+pprint(sol)
+
+# Solution 1:
+# ⎧              2          2          3⎫
+# ⎪   F      -F⋅l        F⋅l      2⋅F⋅l ⎪
+# ⎨A: ─, φₗ: ──────, φᵣ: ────, w: ──────⎬
+# ⎪   6       3⋅EI       3⋅EI      9⋅EI ⎪
+# ⎩                                     ⎭
+#
+# φL, φR / rad:
+# -1/3
+# 1/3
+#
+# w / m:
+# 2/9
+#
+# Solution 2:
+#
+# Stiffness matrix / N:
+# ⎡  18                   ⎤
+# ⎢─────     6       -3   ⎥
+# ⎢meter                  ⎥
+# ⎢                       ⎥
+# ⎢  6    4⋅meter     0   ⎥
+# ⎢                       ⎥
+# ⎣ -3       0     2⋅meter⎦
+# ⎧                         2⋅meter⎫
+# ⎨p2m: -1/3, p2p: 1/3, w₂: ───────⎬
+# ⎩                            9   ⎭
