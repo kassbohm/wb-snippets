3d8cfad1c529f304322f5fa9313a4bc7795501a1
kassbohm <kassbohm@gmail.com>
2021-05-31 07:46:36 +0200

    "tag"

--- py/TEM_10/ESA/a1_cc.py
+++ py/TEM_10/ESA/a1_cc.py
@@ -1,3 +1,62 @@
+# Header starts here.
+from sympy.physics.units import *
+from sympy import *
+
+# Rounding:
+import decimal
+from decimal import Decimal as DX
+from copy import deepcopy
+def iso_round(obj, pv, rounding=decimal.ROUND_HALF_EVEN):
+    import sympy
+    """
+    Rounding acc. to DIN EN ISO 80000-1:2013-08
+    place value = Rundestellenwert
+    """
+    assert pv in set([
+        # place value   #  round to:
+        1,              #  1
+        0.1,            #  1st digit after decimal
+        0.01,           #  2nd
+        0.001,          #  3rd
+        0.0001,         #  4th
+        0.00001,        #  5th
+        0.000001,       #  6th
+        0.0000001,      #  7th
+        0.00000001,     #  8th
+        0.000000001,    #  9th
+        0.0000000001,   # 10th
+        ])
+    objc = deepcopy(obj)
+    try:
+        tmp = DX(str(float(objc)))
+        objc = tmp.quantize(DX(str(pv)), rounding=rounding)
+    except:
+        for i in range(len(objc)):
+            tmp = DX(str(float(objc[i])))
+            objc[i] = tmp.quantize(DX(str(pv)), rounding=rounding)
+    return objc
+
+# LateX:
+kwargs = {}
+kwargs["mat_str"] = "bmatrix"
+kwargs["mat_delim"] = ""
+
+# kwargs["symbol_names"] = {FB: "F^{\mathsf B}", }
+
+# Units:
+(k, M, G ) = ( 10**3, 10**6, 10**9 )
+(mm, cm) = ( m/1000, m/100 )
+Newton = kg*m/s**2
+Pa     = Newton/m**2
+MPa    = M*Pa
+GPa    = G*Pa
+kN     = k*Newton
+deg    = pi/180
+
+half = S(1)/2
+
+# Header ends here.
+#
 # https://colab.research.google.com/github/kassbohm/wb-snippets/blob/master/ipynb/TEM_10/ESA/a1_cc.ipynb
 
 F,l = var("F,l")
